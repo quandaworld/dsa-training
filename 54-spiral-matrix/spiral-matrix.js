e90b010f8ed0ac56b8c73270ac65ab81d5326a55
Quan Nguyen <46980777+quandaworld@users.noreply.github.com>
2024-02-20 18:53:31 -0800

    Time: 55 ms (36.23%) | Memory: 48.5 MB (47.00%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let top = 0;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let bottom = matrix.length - 1;
+    const res = [];
+    const size = matrix[0].length * matrix.length;
+
+    while (res.length < size) {
+        for (let i = left; i <= right && res.length < size; i++) {
+            res.push(matrix[top][i]);
+        }
+        top++;
+
+        for (let i = top; i <= bottom && res.length < size; i++) {
+            res.push(matrix[i][right]);
+        }
+        right--;
+
+        for (let i = right; i >= left && res.length < size; i--) {
+            res.push(matrix[bottom][i]);
+        }
+        bottom--;
+
+        for (let i = bottom; i >= top && res.length < size; i--) {
+            res.push(matrix[i][left]);
+        }
+        left++;
+    }
+
+    return res;
+};
