1861d5c5e1cf3dc5dc6dd7f1b9311d8313233d5c
Quan Nguyen <46980777+quandaworld@users.noreply.github.com>
2024-02-20 22:16:42 -0800

    Time: 59 ms (47.58%) | Memory: 50.5 MB (7.89%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let [l, r] = [0, numbers.length - 1];
+    while (l < r) {
+        if (numbers[l] + numbers[r] < target) {
+            l++;
+        } else if (numbers[l] + numbers[r] > target) {
+            r--;
+        } else {
+            return [l + 1, r + 1];
+        }
+    }
+};
