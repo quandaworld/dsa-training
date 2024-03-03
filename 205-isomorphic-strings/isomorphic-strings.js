b71b7824550d20543b35e036862dfe8828889a50
Quan Nguyen <46980777+quandaworld@users.noreply.github.com>
2024-03-02 22:20:02 -0800

    Time: 58 ms (75.51%) | Memory: 50.3 MB (50.70%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isIsomorphic(s, t) {
+    // If s and t are of different lengths, they cannot be isomorphic
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sToT = new Map();
+    const tToS = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        if (sToT.has(charS)) {
+            if (sToT.get(charS) !== charT) {
+                return false;
+            }
+        } else {
+            sToT.set(charS, charT);
+        }
+
+        if (tToS.has(charT)) {
+            if (tToS.get(charT) !== charS) {
+                return false;
+            }
+        } else {
+            tToS.set(charT, charS);
+        }
+    }
+
+    return true;
+}
+
